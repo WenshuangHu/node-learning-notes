10e3f672981e3a147f64d3b49c2cb0b068b61016
WenshuangHu <huwenshuang@foxmail.com>
2024-08-17 10:30:39 +0800

    learning notes

---
+++ big-event-cms-api/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: 'ren1314',
+  database: 'bigEvent_api',
+  connectionLimit: 2 //创建的连接池个数
+}
---
+++ big-event-cms-api/routes/articleCase.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+
+const articleCaseController = require('../controllers/articleCaseController')
+
+const expressJoi = require('../schemas/express-joi')
+const {
+  addCate_schema,
+  deleteCate_schema,
+  getCate_schema,
+  updateCate_schema
+} = require('../schemas/articleCaseSchema')
+
+// 1、获取-文章分类（查询所有）
+router.get('/list', articleCaseController.getArtCateList)
+
+// 2、增加-文章分类
+router.post(
+  '/add',
+  expressJoi(addCate_schema),
+  articleCaseController.addArtCate
+)
+
+// 3、获取-文章分类详情（根据id值）
+router.get(
+  '/info',
+  expressJoi(getCate_schema),
+  articleCaseController.getArticleDetail
+)
+
+// 4、更新-文章分类
+router.put(
+  '/info',
+  expressJoi(updateCate_schema),
+  articleCaseController.updateCate
+)
+
+// 5、删除-文章分类
+router.delete(
+  '/del',
+  expressJoi(deleteCate_schema),
+  articleCaseController.delArtCateList
+)
+
+module.exports = router
